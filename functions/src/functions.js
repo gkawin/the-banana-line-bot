1183b78c49f18f7c9589a7efaee88c0fed4cc35b
Kawin Glomjai <kawin.g@taskworld.com>
2017-04-08 10:35:13 +0700

    set zero

--- functions/src/functions.js
+++
@@ -1,9 +0,0 @@
-import * as Functions from 'firebase-functions'
-// import * as Admin from 'firebase-admin'
-// import uuid from 'uuid'
-
-// Admin.initializeApp(Functions.config().firebase)
-
-exports.bot = Functions.https.onRequest((req, res) => {
-  res.send('99999')
-})
