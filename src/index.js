4738215ebb22a39b109d686017e457438f39b3ac
Kawin Glomjai <kawin.g@taskworld.com>
2017-04-08 12:28:21 +0700

    hello world expres

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-export const hello = () => 'hello' 
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import morgan from 'morgan'
+import helmet from 'helmet'
+
+const app = express()
+
+//
+app.use(morgan('dev'))
+app.use(cors())
+app.use(bodyParser.json())
+app.use(helmet)
+
+app.get('/', (req, res) => {
+  res.status(200).json('hello world')
+})
+
+app.listen(3000, () => {
+  console.log('Sever is up!!!!!')
+})
