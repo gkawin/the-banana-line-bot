a975d9f9a93d780aa23bbfc650839ddf0ab42807
Kawin Glomjai <kawin.g@taskworld.com>
2017-04-16 18:46:32 +0700

    try to spread reply logic

--- src/webhook/index.js
+++ src/webhook/index.js
@@ -3,27 +3,31 @@ import Boom from 'boom'
 import * as CheckRequest from './checkRequest'
 import * as Services from '../services'
 
+class ReplyMessage {
+  constructor (replyToken) {
+    this._replyToken = replyToken
+  }
+  getMenu () {
+    return {
+      replyToken: this._replyToken,
+      messages: [{
+        type: 'text',
+        text: `รายละเอียดคอร์ส พิมพ์ 1/n
+        สอบถามราคาและโปรโมชั่น พิมพ์ 2/n`
+      }]
+    }
+  }
+}
+
 export default async function webhook (request: Object, response: Object, next: Function) {
   const validate = CheckRequest.isValidation(request)
   if (!validate) next(Boom.badRequest())
 
   for (const event of request.body.events) {
-    const replyToken = event.replyToken
-    const messages = {
-      replyToken: replyToken,
-      messages: [
-        {
-          'type': 'text',
-          'text': `Hello ${replyToken}`
-        },
-        {
-          'type': 'text',
-          'text': 'May I help you?'
-        }
-      ]
-    }
+    const replyMessage = new ReplyMessage(event.replyToken)
+    console.log(replyMessage.getMenu())
     try {
-      await Services.reply.message(messages)
+      await Services.reply.message(replyMessage.getMenu())
     } catch (e) {
       next(e)
     }
