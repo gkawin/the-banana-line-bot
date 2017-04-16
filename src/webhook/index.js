90aa708b59909ba2d0c047eaeb166db52af1ccce
Kawin Glomjai <kawin.g@taskworld.com>
2017-04-16 11:20:31 +0700

    handle every events

--- src/webhook/index.js
+++ src/webhook/index.js
@@ -7,14 +7,14 @@ export default async function webhook (request: Object, response: Object, next: 
   const validate = CheckRequest.isValidation(request)
   if (!validate) next(Boom.badRequest())
 
-  try {
-    const replyToken = request.body.events[0].replyToken
+  for (const event of request.body.events) {
+    const replyToken = event.replyToken
     const messages = {
       replyToken: replyToken,
       messages: [
         {
           'type': 'text',
-          'text': 'Hello, Refactor now'
+          'text': `Hello ${replyToken}`
         },
         {
           'type': 'text',
@@ -22,8 +22,10 @@ export default async function webhook (request: Object, response: Object, next: 
         }
       ]
     }
-    await Services.reply.message(messages)
-  } catch (e) {
-    next(e)
+    try {
+      await Services.reply.message(messages)
+    } catch (e) {
+      next(e)
+    }
   }
 }
