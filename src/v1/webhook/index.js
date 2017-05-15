652d0e152c4695bd20748f55701ffa4adb585a52
Kawin Glomjai <kawin.g@taskworld.com>
2017-05-15 18:21:09 +0700

    error handler

--- src/v1/webhook/index.js
+++ src/v1/webhook/index.js
@@ -1,9 +1,24 @@
+import { Client } from '@line/bot-sdk'
 import Boom from 'boom'
 import _ from 'lodash'
+
+import Config from '../../Config'
 
 export default async (req: Object, res: Object, next: Function) => {
   const validate = !(_.isEmpty(req.body))
   if (!validate) next(Boom.badRequest())
-  console.log(req.body)
-  res.json('fuck')
+
+  const client = new Client(Config.LINE)
+  const events = req.body.events
+
+  for (const event of events) {
+    try {
+      await client.replyMessage(event.replyToken, {
+        type: 'text',
+        message: 'heelo from line sdk'
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
 }
